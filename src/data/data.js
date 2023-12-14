33ba9f1dd8ea035537b7a35e95253e4df883179b
JarJarDinks <Cochranejacob22@gmail.com>
2023-12-14 11:00:05 -0700

    update project

--- src/data/data.js
+++ src/data/data.js
@@ -2,51 +2,51 @@ import AngularMovieApp from '../assets/projects/angularmovieapp.png';
 import PokemonIndex from '../assets/projects/pokemonindex.png';
 import chatschatapp from '../assets/projects/chatschatapp.png';
 import mymovieapi from '../assets/projects/mymovieapi.png';
-import reactmovieapp from '../assets/projects/reactmovieapp.png';
+import devmeetups from '../assets/projects/devmeetups.jpg';
 import meetapp from '../assets/projects/meetapp.png';
 import tastebuds from '../assets/projects/tastebuds.jpg';
 
 export const data = [
   {
     id: 1,
+    name: 'React Dev Meetup App',
+    image: devmeetups,
+    github: 'https://github.com/JarJarDinks/Dev-Meetups/',
+    live: 'https://jarjardinks.github.io/Dev-Meetups/',
+  },
+  {
+    id: 2,
     name: 'Angular Movie App',
     image: AngularMovieApp,
     github: 'https://github.com/JarJarDinks/myFLix-Angular-client',
     live: 'https://jarjardinks.github.io/myFLix-Angular-client/welcome',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Django Recipe App',
     image: tastebuds,
     github: 'https://github.com/JarJarDinks/recipe-app',
     live: 'https://shrouded-forest-61463-f52cd1412c70.herokuapp.com/',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Vanilla JS Pokemon Index',
     image: PokemonIndex,
     github: 'https://github.com/JarJarDinks/pokemon-index',
     live: 'https://jarjardinks.github.io/pokemon-index/',
   },
   {
-    id: 4,
+    id: 5,
     name: 'React Native Chat App',
     image: chatschatapp,
     github: 'https://github.com/JarJarDinks/ChatApp',
     live: '',
   },
   {
-    id: 5,
+    id: 6,
     name: 'Node & Express API',
     image: mymovieapi,
     github: 'https://github.com/JarJarDinks/myFlix-API',
     live: 'https://mymovieapidb.herokuapp.com/',
-  },
-  {
-    id: 6,
-    name: 'React Meet App',
-    image: meetapp,
-    github: 'https://github.com/JarJarDinks/meet-app',
-    live: 'https://jarjardinks.github.io/meet-app/',
-  },
+  }
 ];
