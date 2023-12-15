5c71e7ee3e24f7ab580066c44a5b4abca7baa120
JarJarDinks <Cochranejacob22@gmail.com>
2023-12-15 11:05:01 -0700

    typo in p tag

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
               I'm a passionate software developer based in Arizona. I build web
               apps that are both vissually stunning and user-friendly. I'm
               committed to delivering high-quality solutions that exceed
-              expectations. With a focus on innovation and problem solving. I'm
+              expectations, with a focus on innovation and problem solving. I'm
               dedicated to helping businesses and indibiduals achieve their
               digital goals.
             </p>
