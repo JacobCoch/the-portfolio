54ff2c47b47ff280c98973277ab47b21fb8a27e3
JarJarDinks <Cochranejacob22@gmail.com>
2023-12-27 19:20:51 -0700

    update github username

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -123,7 +123,7 @@ function NavBar() {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300 pl-3'
-                            href='https://github.com/JarJarDinks'
+                            href='https://github.com/JacobCoch'
                             target='_blank'
                             rel='noreferrer'
                         >
